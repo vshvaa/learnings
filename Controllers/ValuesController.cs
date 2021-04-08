e5dd3dff8f3857bc442d3f9e9d06f11027619c69
Shrivalli <shrivalli04@gmail.com>
2021-04-08 10:50:17 +0530

    Value8 Added

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace Dockersampleconfig.Controllers
             l.Add("Value5");
             l.Add("Value6");
             l.Add("Value7");
+            l.Add("Value8);
             return l;
         }
     }
